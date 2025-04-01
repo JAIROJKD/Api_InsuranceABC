5d5c2d627fe255ccd38b4ec79a3e9b0d40451fed
JAIROJKD <jairojkd@hotmail.com>
2025-04-01 12:49:30 -0500

    limpieza de archivos innecesarios y actualizarcion de program

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ?? Habilita Swagger para documentación
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ?? Configura la conexión a la base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -13,12 +15,17 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// ?? Configuración en modo desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// ?? Redirigir HTTP a HTTPS
+app.UseHttpsRedirection();
+app.UseCors("AllowAngularApp");
 app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
